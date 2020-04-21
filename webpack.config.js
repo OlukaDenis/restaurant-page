cfc391c2e25819de5ff43c97c164fd320b788288
OlukaDenis <olukadeno@gmail.com>
2020-04-21 10:35:38 +0300

    Added the babelrc and added the rules to the webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,13 @@ module.exports = {
           'file-loader',
         ],
       },
+      {
+        test: /\.js$/, //using regex to tell babel exactly what files to transcompile
+        exclude: /node_modules/, // files to be ignored
+        use: {
+            loader: 'babel-loader' // specify the loader
+        } 
+    }
     ],
   }
 };
