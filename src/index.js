b09faeab0a0bcc687d36c77a8d423c63c97fae51
OlukaDenis <olukadeno@gmail.com>
2020-04-21 17:01:55 +0300

    completed the hompage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './index.css';
-import bgImage from './images/bg.jpg'
+import home from './modules/home';
 
-let element = `
-  <div class="element">
-    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur laudantium recusandae itaque libero velit minus ex reiciendis veniam. Eligendi modi sint
-  delectus beatae nemo provident ratione maiores, voluptatibus a tempore!</h1>
-  <div class="image"></div>
-  </div>
-`
+const content = document.getElementById('content');
 
-document.write(element);
+const render = () => {
+  content.innerHTML = '';
+  content.appendChild(home());
+};
+
+render();
---
+++ src/modules/home.js
@@ -0,0 +1,23 @@
+const home = () => {
+  const items = document.createElement('div');
+  items.id = 'homepage';
+  items.classList.add('homepage');
+
+  items.innerHTML = `
+    <div class="shadow"></div>
+  `
+  
+  const container = document.createElement('div');
+  container.classList.add('home-container');
+  container.innerHTML = `
+    <h1> Denny's Restaurant </h1>
+    <p> Eat, Drink </p>
+    <p> - Be Merry </p>
+  `
+  items.appendChild(container);
+
+
+  return items;
+}
+
+export default home;
