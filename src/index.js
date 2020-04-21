f9d82b7a29fba8b1c87889a1d913984107236255
OlukaDenis <olukadeno@gmail.com>
2020-04-21 10:17:49 +0300

    Added the css and image loaders to webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './index.css';
+import bgImage from './images/bg.jpg'
+
+let element = `
+  <div class="element">
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur laudantium recusandae itaque libero velit minus ex reiciendis veniam. Eligendi modi sint
+  delectus beatae nemo provident ratione maiores, voluptatibus a tempore!</h1>
+  <div class="image"></div>
+  </div>
+`
+
+document.write(element);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+            'css-loader',
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  }
+};
