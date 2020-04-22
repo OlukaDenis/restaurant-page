bbb8da7ac5bbced1a523c5bde9ba2275f7376eaf
OlukaDenis <olukadeno@gmail.com>
2020-04-22 20:07:22 +0300

    Added the fontawesome dependency

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,7 +3,6 @@ import menuList from '../data/menu_list';
 const mealList = (meal) => {
   const menuRow = document.createElement('div');
   menuRow.setAttribute('class', 'meal-items');
-
   menuRow.innerHTML
   = `
     <div> 
@@ -11,31 +10,25 @@ const mealList = (meal) => {
     </div>
     <p> $ ${meal.price} </p>
   `
-
   return menuRow;
 };
 
 const menuData = (element) => {
   const content = document.createElement('div');
   content.setAttribute('class', 'meal');
-  content.innerHTML = `<h5> ${element.type} </h5>`; 
-  
+  content.innerHTML = `<h5> ${element.type} </h5>`;  
   element.items.forEach( meal => {
     content.appendChild(mealList(meal));
   });
   return content;
 };
 
-
 const menu = () => {
   const items = document.createElement('div');
-
   items.id = 'menupage';
   items.classList.add('menupage');
-
   const container = document.createElement('div');
   container.classList.add('menu-container');
-
   container.innerHTML = `
     <h3> Our Menu </h3>
   `
@@ -47,12 +40,8 @@ const menu = () => {
   });
   
   container.appendChild(content);
-
   items.appendChild(container);
-
   return items;
 };
-
-
 
 export default menu;
