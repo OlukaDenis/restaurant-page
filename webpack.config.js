644a475729ff4f62b58d439a55d0ecfc0c8d5a1a
OlukaDenis <olukadeno@gmail.com>
2020-04-23 00:53:46 +0300

    Fixed linting errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src",
+  entry: './src',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
     ],
